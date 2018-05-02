d72469845053736a6ff44d2cec4d138d318e808a
ISEM\renaud.lebrun <renaud.lebrun@umontpellier.fr>
2018-05-02 16:15:07 +0200

    Minimal example works now... mainly a Cmain issue eventually...

--- MorphoDig/main.cxx
+++ MorphoDig/main.cxx
@@ -7,6 +7,7 @@
  * statement of authorship are reproduced on all copies.
  */
 // QT includes
+
 #include <QApplication>
 #include <QSurfaceFormat>
 #include <QVTKOpenGLWidget.h>
@@ -18,6 +19,10 @@
 #endif
 #include "mqMinimalWidget.h"
 #include "MorphoDig.h"
+
+// @@
+
+//@@
 
 extern int qInitResources_icons();
 
@@ -36,9 +41,9 @@ int main( int argc, char* argv[] )
   #else
     QApplication::setStyle(new QCleanlooksStyle);
   #endif
-
+  
   qInitResources_icons();
-
+  
   //mqMinimalWidget mymin;
   //mymin.show();
   
@@ -46,5 +51,98 @@ int main( int argc, char* argv[] )
   MorphoDig myMorphoDig;
   myMorphoDig.show();
 
+  // Create a table with some points in it
+
   return app.exec();
 }
+/*
+
+//Just for some tests implying vtkChartXY which was not working for a long while... 
+
+int main( int argc, char* argv[] )
+{
+#include <vtkVersion.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderWindow.h>
+#include <vtkSmartPointer.h>
+#include <vtkChartXY.h>
+#include <vtkTable.h>
+#include <vtkPlot.h>
+#include <vtkFloatArray.h>
+#include <vtkContextView.h>
+#include <vtkContextScene.h>
+#include <vtkPen.h>
+
+
+vtkSmartPointer<vtkTable> table =
+vtkSmartPointer<vtkTable>::New();
+
+vtkSmartPointer<vtkFloatArray> arrX =
+vtkSmartPointer<vtkFloatArray>::New();
+arrX->SetName("X Axis");
+table->AddColumn(arrX);
+
+vtkSmartPointer<vtkFloatArray> arrC =
+vtkSmartPointer<vtkFloatArray>::New();
+arrC->SetName("Cosine");
+table->AddColumn(arrC);
+
+vtkSmartPointer<vtkFloatArray> arrS =
+vtkSmartPointer<vtkFloatArray>::New();
+arrS->SetName("Sine");
+table->AddColumn(arrS);
+
+// Fill in the table with some example values
+int numPoints = 69;
+float inc = 7.5 / (numPoints - 1);
+table->SetNumberOfRows(numPoints);
+for (int i = 0; i < numPoints; ++i)
+{
+table->SetValue(i, 0, i * inc);
+table->SetValue(i, 1, cos(i * inc));
+table->SetValue(i, 2, sin(i * inc));
+}
+
+// Set up the view
+vtkSmartPointer<vtkContextView> view =
+vtkSmartPointer<vtkContextView>::New();
+view->GetRenderer()->SetBackground(1.0, 1.0, 1.0);
+
+// Add multiple line plots, setting the colors etc
+vtkSmartPointer<vtkChartXY> chart =
+vtkSmartPointer<vtkChartXY>::New();
+view->GetScene()->AddItem(chart);
+vtkPlot *line = chart->AddPlot(vtkChart::LINE);
+#if VTK_MAJOR_VERSION <= 5
+line->SetInput(table, 0, 1);
+#else
+line->SetInputData(table, 0, 1);
+#endif
+line->SetColor(0, 255, 0, 255);
+line->SetWidth(1.0);
+line = chart->AddPlot(vtkChart::LINE);
+#if VTK_MAJOR_VERSION <= 5
+line->SetInput(table, 0, 2);
+#else
+line->SetInputData(table, 0, 2);
+#endif
+line->SetColor(255, 0, 0, 255);
+line->SetWidth(5.0);
+
+// For dotted line, the line type can be from 2 to 5 for different dash/dot
+// patterns (see enum in vtkPen containing DASH_LINE, value 2):
+#ifndef WIN32
+line->GetPen()->SetLineType(vtkPen::DASH_LINE);
+#endif
+// (ifdef-ed out on Windows because DASH_LINE does not work on Windows
+//  machines with built-in Intel HD graphics card...)
+
+//view->GetRenderWindow()->SetMultiSamples(0);
+
+// Start interactor
+view->GetInteractor()->Initialize();
+view->GetInteractor()->Start();
+return EXIT_SUCCESS;
+}
+*/
