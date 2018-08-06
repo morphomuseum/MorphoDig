eab0c5065ac547c6dd06933dc10519a5d03801c4
ISEM\renaud.lebrun <renaud_lebrun@yahoo.fr>
2018-08-06 16:34:43 +0200

    Not much...

--- MorphoDig/Qt/mqMorphoDigMenuBuilders.cxx
+++ MorphoDig/Qt/mqMorphoDigMenuBuilders.cxx
@@ -155,7 +155,7 @@ void mqMorphoDigMenuBuilders::buildFileMenu(QMenu& menu)
   new mqOpenDataReaction(submenuColorMaps->addAction("Import color maps (MAP)") << mqSetName("actionOpenMAP"), 17);
   new mqSaveMAPDialogReaction(submenuColorMaps->addAction("Export color maps (MAP)") << mqSetName("actionSaveMAP"));
   
-  new mqOpenDataReaction(submenuTags->addAction("Import tag maps (.TGP, .TAG)") << mqSetName("actionOpenTAGMAP"), 9);
+  new mqOpenDataReaction(submenuTags->addAction("Import tag maps (.TGP or .TAG)") << mqSetName("actionOpenTAGMAP"), 9);
   new mqSaveTAGMAPDialogReaction(submenuTags->addAction("Export tag maps (.TGP or .TAG)") << mqSetName("actionSaveTAGMAP"));
   
 
