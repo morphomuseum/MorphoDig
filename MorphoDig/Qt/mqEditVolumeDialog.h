7e9be4d7b4c01cf09c09340f2d022f8b29204448
renaud-lebrun <renaud_lebrun@yahoo.fr>
2019-04-15 16:44:53 +0200

    New volume dialog

---
+++ MorphoDig/Qt/mqEditVolumeDialog.h
@@ -0,0 +1,71 @@
+/*=========================================================================
+
+   Program: MorphoDig
+   
+
+=========================================================================*/
+#ifndef _mqEditACTORDialog_h
+#define _mqEditACTORDialog_h
+
+#include "vtkMDActor.h"
+#include "vtkMDActorCollection.h"
+#include <vtkSmartPointer.h>
+#include <QDialog>
+
+namespace Ui
+{
+class mqEditACTORDialog;
+}
+
+
+
+
+/**
+* mqEditACTORDialog is the dialog used by MorphoDig to save CUR files.
+*/
+class  mqEditACTORDialog : public QDialog
+{
+  Q_OBJECT
+
+public:
+  mqEditACTORDialog(QWidget* Parent);
+  
+  ~mqEditACTORDialog();
+  void saveActor();
+  void GetFirstSelectedActor();
+  void GetFirstActor();
+  void GetNextActor();
+  void GetPrecedingActor();
+  void RefreshNumCellsNumPoints();
+  void UpdateUI();
+  int SomeThingHasChanged();
+  int CurrentActorInCollection();
+  void RefreshDialog();
+  public slots:
+  
+  virtual void slotRefreshDialog();
+  virtual void slotGetPrecedingActor();
+  virtual void slotGetNextActor();
+  virtual void slotsaveActor();
+  virtual void slotReinitMatrix();
+  virtual void slotapplyMatrixToAllSelectedActors();
+  virtual void slotEditScalar();
+  virtual void slotDuplicateScalar();
+  virtual void slotDeleteScalar();
+
+protected:
+	
+private:
+	
+
+  Q_DISABLE_COPY(mqEditACTORDialog)
+  Ui::mqEditACTORDialog* const Ui;
+  vtkSmartPointer<vtkMDActor> ACTOR;
+  //vtkMDActor *ACTOR;
+  vtkMDActorCollection *ACTOR_Coll;
+  int current_coll;
+
+  // Here we should have the file name, no ?
+};
+
+#endif // !_mqCURDialog_h
---
+++ MorphoDig/Qt/mqEditVolumeDialogReaction.cxx
@@ -0,0 +1,30 @@
+/*=========================================================================
+
+   Program: MorphoDig
+   Module:    mqEditACTORDialogReaction.cxx
+
+
+========================================================================*/
+#include "mqEditACTORDialogReaction.h"
+#include "mqCoreUtilities.h"
+#include "mqEditACTORDialog.h"
+#include "mqReaction.h"
+
+
+//-----------------------------------------------------------------------------
+mqEditACTORDialogReaction::mqEditACTORDialogReaction(QAction* parentObject)
+  : Superclass(parentObject)
+{
+	this->ACTOR_dialog = new mqEditACTORDialog(mqCoreUtilities::mainWidget());
+
+	
+}
+
+
+//-----------------------------------------------------------------------------
+void mqEditACTORDialogReaction::showEditACTORDialog(mqEditACTORDialog *ACTOR_dialog)
+{
+	
+	ACTOR_dialog->RefreshDialog();
+  ACTOR_dialog->show();
+}
